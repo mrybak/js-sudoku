6af5191bd27a16b23b770f925a14a71f7032ca60
Michał Rybak <kontakt@mrybak.pl>
2013-03-08 13:24:47 +0000

    directories reorganized

--- script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	// Initialize application
-	Sudoku.init();
-	
-	// Load riddle
-	var boardNumber = Math.floor(Math.random()*10);
-	Sudoku.loadBoard(boards[0], boards[0], solutions[0]);
-});
